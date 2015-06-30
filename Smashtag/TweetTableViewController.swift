e831263bc557f78719594bc7b1e8aaad8d280818
Dmitry Terekhov <dimanter@gmail.com>
2015-06-30 22:05:27 +0300

    Initial commit

---
+++ Smashtag/TweetTableViewController.swift
@@ -0,0 +1,113 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewController: UITableViewController, UITextFieldDelegate
+{
+    // MARK: - Public API
+
+    var tweets = [[Tweet]]()
+
+    var searchText: String? = "#stanford" {
+        didSet {
+            lastSuccessfulRequest = nil
+            searchTextField?.text = searchText
+            tweets.removeAll()
+            tableView.reloadData() // clear out the table view
+            refresh()
+        }
+    }
+    
+    // MARK: - View Controller Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = tableView.rowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+        refresh()
+    }
+    
+    // MARK: - Refreshing
+
+    private var lastSuccessfulRequest: TwitterRequest?
+
+    private var nextRequestToAttempt: TwitterRequest? {
+        if lastSuccessfulRequest == nil {
+            if searchText != nil {
+                return TwitterRequest(search: searchText!, count: 100)
+            } else {
+                return nil
+            }
+        } else {
+            return lastSuccessfulRequest!.requestForNewer
+        }
+    }
+    
+    @IBAction private func refresh(sender: UIRefreshControl?) {
+        if let request = nextRequestToAttempt {
+            request.fetchTweets { (newTweets) -> Void in
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    if newTweets.count > 0 {
+                        self.lastSuccessfulRequest = request // oops, forgot this line in lecture
+                        self.tweets.insert(newTweets, atIndex: 0)
+                        self.tableView.reloadData()
+                    }
+                    sender?.endRefreshing()
+                }
+            }
+        } else {
+            sender?.endRefreshing()
+        }
+    }
+    
+    func refresh() {
+        refreshControl?.beginRefreshing()
+        refresh(refreshControl)
+    }
+    
+    // MARK: - Storyboard Connectivity
+    
+    @IBOutlet private weak var searchTextField: UITextField! {
+        didSet {
+            searchTextField.delegate = self
+            searchTextField.text = searchText
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == searchTextField {
+            textField.resignFirstResponder()
+            searchText = textField.text
+        }
+        return true
+    }
+    
+    private struct Storyboard {
+        static let CellReuseIdentifier = "Tweet"
+    }
+    
+    // MARK: - UITableViewDataSource
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return tweets.count
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets[section].count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIdentifier, forIndexPath: indexPath) as! TweetTableViewCell
+
+        cell.tweet = tweets[indexPath.section][indexPath.row]
+
+        return cell
+    }
+}
