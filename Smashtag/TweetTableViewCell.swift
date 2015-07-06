aa8d628aebaf9bad4935cf45b699d45a8b72f863
Dmitry Terekhov <dimanter@gmail.com>
2015-07-06 09:43:53 +0300

    Fix block main thread during load image for every cell

--- Smashtag/TweetTableViewCell.swift
+++ Smashtag/TweetTableViewCell.swift
@@ -21,11 +21,13 @@ class TweetTableViewCell: UITableViewCell
     @IBOutlet weak var tweetTextLabel: UILabel!
     @IBOutlet weak var tweetCreatedLabel: UILabel!
     
+    let profilePlaceholderImage = UIImage(named: "ProfilePlaceholderImg")
+    
     func updateUI() {
         // reset any existing tweet information
         tweetTextLabel?.attributedText = nil
         tweetScreenNameLabel?.text = nil
-        tweetProfileImageView?.image = nil
+        tweetProfileImageView?.image = profilePlaceholderImage
         tweetCreatedLabel?.text = nil
         
         // load new information from our tweet (if any)
@@ -41,8 +43,16 @@ class TweetTableViewCell: UITableViewCell
             tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
             
             if let profileImageURL = tweet.user.profileImageURL {
-                if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread!
-                    tweetProfileImageView?.image = UIImage(data: imageData)
+                let qos = Int(QOS_CLASS_USER_INITIATED.value)
+                dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
+                    let profileImageData = NSData(contentsOfURL: profileImageURL)
+                    dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                        if profileImageURL == tweet.user.profileImageURL && profileImageData != nil {
+                            self.tweetProfileImageView.image = UIImage(data: profileImageData!)
+                        } else {
+                            self.tweetProfileImageView.image = self.profilePlaceholderImage
+                        }
+                    }
                 }
             }
             
